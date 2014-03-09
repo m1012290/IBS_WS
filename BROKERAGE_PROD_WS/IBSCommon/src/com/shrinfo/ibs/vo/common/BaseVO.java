b95c0950833a3499f77544126b0dfd5086cab8e0
m1012290 <hosur.sunil@gmail.com>
2014-03-10 02:40:12 +0530

    Changes name of the created
    twst

--- BROKERAGE_PROD_WS/IBSCommon/src/com/shrinfo/ibs/vo/common/BaseVO.java
+++ BROKERAGE_PROD_WS/IBSCommon/src/com/shrinfo/ibs/vo/common/BaseVO.java
@@ -10,7 +10,7 @@ import java.util.Date;
  *
  */
 public class BaseVO {
-	private long createdByUserId;
+	private long createdByUserId1;
 	private Date createdDate;
 	private long updatedByUserId;
 	private Date updatedDate;
